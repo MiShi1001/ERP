54dc1efcc7f04b225d22cb645d2a035cb03e90db
SU-RU <oooaa0727@gmail.com>
2017-12-17 21:28:29 +0800

    test : login name OK

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,16 @@
 @section('content')
 
 
+    <div class="panel-body">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        You are logged in!
+    </div>
+
 <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
